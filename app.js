8d93f27e2286c99af3b30e1147d6016bcd11d4cd
khaledjamal1 <khalidfaqari@gmail.com>
2022-04-13 16:14:38 +0300

    done

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
-// oeprations
-function add(num1,num2){
-    return num1+num2
-}
-function sub(num1,num2){
-    return num1-num2
-}
-function multi(num1,num2){
-    return num1*num2
-}
-function divide(num1,num2){
-    return num1/num2
-}
+
 // buttons function
-let num1 =+ numb()
-let num2 = ""
-let operation = oper()
-function numb(){
-    const numk = document.querySelectorAll(".number")
-    numk.forEach(number => number.addEventListener("click", (e)=>{
-        current.textContent += (e.target.innerText)
-    }))
-}
-function oper(){
-    const op = document.querySelectorAll(".operation")
-    op.forEach(operation => operation.addEventListener("click", (e)=>{
-        return (e.target.outerText)
-        console.log(e.target.outerText)
-    }))
-}
-// cal
-const scr = document.querySelector(".output")
-const pre = document.querySelector(".pre")
+let currentnum = ""
+let prenum = ""
+let operation = ""
+
+
+
+const numk = document.querySelectorAll(".number")
+const oper = document.querySelectorAll(".operation")
 const current = document.querySelector(".current")
-function test1(){
-    let test = numb()
-    console.log(test)
+const pred = document.querySelector(".pre")
+const equal = document.querySelector(".equal")
+const del = document.querySelector(".delete")
+
+
+numk.forEach(Number => Number.addEventListener("click", (e) =>{
+    handlenumber(e.target.textContent)
+}))
+function handlenumber(number){
+    currentnum += number 
+    current.textContent = currentnum
 }
-
-
-numb()
+oper.forEach(operation => operation.addEventListener("click", (e) =>{
+    handleoperation(e.target.textContent)
+}))
+function handleoperation(op){
+    operation = op 
+    prenum = currentnum
+    currentnum = ""
+    current.textContent = ""
+    pred.textContent = prenum + op
+}
+equal.addEventListener("click", calculate)
+function calculate() {
+    prenum = Number(prenum);
+    currentnum = Number(currentnum);
+    if (operation === "+"){
+        prenum = prenum + currentnum
+    } else if (operation === "-"){
+        prenum = prenum - currentnum
+    } else if (operation === "/"){
+        prenum = prenum / currentnum  
+    } else if (operation === "x"){
+        prenum = prenum * currentnum
+    }
+    currentnum = prenum
+    current.textContent = prenum
+    pred.textContent = ""
+}
+const ac = document.querySelector(".ac")
+ac.addEventListener("click", () =>{
+    currentnum = ""
+    prenum = ""
+    current.textContent = ""
+    pred.textContent = ""
+})
+del.addEventListener("click",dele)
+function dele(){
+    currentnum = currentnum.slice(0,-1)
+    current.textContent = currentnum
+}
