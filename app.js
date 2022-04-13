33c7ca897b07c29076b7bb336b14166994cc75d3
Khaledjamal1 <khalidfaqari@gmail.com>
2022-04-14 00:50:57 +0300

    update

--- app.js
+++ app.js
@@ -12,22 +12,30 @@ function divide(num1,num2){
     return num1/num2
 }
 // buttons function
-let num1 = ""
+let num1 =+ numb()
 let num2 = ""
-let oepration = ""
+let operation = oper()
 function numb(){
     const numk = document.querySelectorAll(".number")
     numk.forEach(number => number.addEventListener("click", (e)=>{
-        console.log (parseFloat(e.target.outerText))
+        current.textContent += (e.target.innerText)
     }))
 }
 function oper(){
     const op = document.querySelectorAll(".operation")
     op.forEach(operation => operation.addEventListener("click", (e)=>{
         return (e.target.outerText)
+        console.log(e.target.outerText)
     }))
 }
-
-function cal(num1,operation,num2){
-    
+// cal
+const scr = document.querySelector(".output")
+const pre = document.querySelector(".pre")
+const current = document.querySelector(".current")
+function test1(){
+    let test = numb()
+    console.log(test)
 }
+
+
+numb()
